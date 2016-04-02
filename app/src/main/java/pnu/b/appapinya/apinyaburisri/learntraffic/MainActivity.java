eab71d11b0488f444675ede5ac3e005c4897aa0a
kinggy <kinggy_hoo@hotmail.com>
2016-04-02 11:56:55 +0700

    test

---
+++ app/src/main/java/pnu/b/appapinya/apinyaburisri/learntraffic/MainActivity.java
@@ -0,0 +1,13 @@
+package pnu.b.appapinya.apinyaburisri.learntraffic;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
