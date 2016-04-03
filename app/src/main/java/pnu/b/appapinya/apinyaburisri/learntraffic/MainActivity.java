d8101fda6a97c8ebefe876203dc3fa67bab196b6
kinggy <kinggy_hoo@hotmail.com>
2016-04-03 09:49:56 +0700

    Intent to WebView

--- app/src/main/java/pnu/b/appapinya/apinyaburisri/learntraffic/MainActivity.java
+++ app/src/main/java/pnu/b/appapinya/apinyaburisri/learntraffic/MainActivity.java
@@ -1,6 +1,9 @@
 package pnu.b.appapinya.apinyaburisri.learntraffic;
 
+import android.content.Intent;
+import android.content.IntentFilter;
 import android.media.MediaPlayer;
+import android.net.Uri;
 import android.provider.LiveFolders;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -27,6 +30,12 @@ public class MainActivity extends AppCompatActivity {
                 //Sound Effect
                 MediaPlayer mediaPlayer = MediaPlayer.create(getBaseContext(),R.raw.lion);
                 mediaPlayer.start();
+
+                //Web View
+                Intent intent = new Intent(Intent.ACTION_VIEW);
+                intent.setData(Uri.parse("https://youtu.be/RCdx7uCZQdA"));
+                startActivity(intent);
+
             }// onClick
         });
 
